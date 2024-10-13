7d61c7887c9da17bb956e7a2ee8c5411be816c6c
Adam Nisenson <144464774+anisenson@users.noreply.github.com>
2024-10-13 15:10:42 +0000

    added down arrow button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $('#headerText2').delay(2500).show(100);
+    $('.arrow').delay(2500).show(100);
+
     $('.spider').delay(2500).fadeIn(3000);
     $('#headerText').fadeIn(3000); 
-
 });
